10df08d0c85f47f80b5a092fa9269c320d493792
周小燕 <zhouxiaoyan@ccbtrust.com.cn>
2020-09-16 16:08:34 +0800

    flask 获取请求的URL

---
+++ flask/request/get_request_url.py
@@ -0,0 +1,13 @@
+# from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify,Response
+# http://127.0.0.1:5000/test/a?x=1
+# request.path: /test/a
+# request.host: 127.0.0.1:5000
+# request.host_url: http://127.0.0.1:5000/
+# request.full_path: /test/a?x=1
+# request.script_root:
+# request.url: http://127.0.0.1:5000/test/a?x=1
+# request.base_url: http://127.0.0.1:5000/test/a
+# request.url_root: http://127.0.0.1:5000/
+
+
+# 来自：https://www.cnblogs.com/lyg-blog/p/10049086.html
