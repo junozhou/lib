a1bc31b07aeb0d3fbbdd405076034fa7c70cbb11
周小燕 <zhouxiaoyan@ccbtrust.com.cn>
2020-09-16 13:55:04 +0800

    pip 安装依赖提示权限不够

---
+++ python/install-issues/permission-denied.sh
@@ -0,0 +1,13 @@
+## error: could not create '/Library/Python/2.7/site-packages/jedi': Permission denied
+#Mark：
+#
+#（1）命令
+pip install spyder
+
+#（2）出错，错误为：
+#error: could not create '/Library/Python/2.7/site-packages/jedi': Permission denied
+
+#（3）解决方法：
+pip install spyder --user
+
+
