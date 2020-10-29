0defaf6dedf96aca190bf302b1f8ebd557fbe310
周小燕 <zhouxiaoyan@ccbtrust.com.cn>
2020-10-29 11:09:18 +0800

    元组转字典.py

---
+++ python/dict/元组转字典.py
@@ -0,0 +1,14 @@
+s = [('yellow', 1), ('blue', 2),  ('red', 1)]
+d = {}
+for k, v in s:
+    d.setdefault(k, v)
+
+d.items()
+
+#
+s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
+d = {}
+for k, v in s:
+    d.setdefault(k, []).append(v)
+
+d.items()
