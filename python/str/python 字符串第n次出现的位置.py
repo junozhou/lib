21ebd0c8dfe1a8cdfbd0e0cf9fee5aaab66311f5
周小燕 <zhouxiaoyan@ccbtrust.com.cn>
2020-11-02 09:49:14 +0800

    python 字符串第n次出现的位置

---
+++ python/str/python 字符串第n次出现的位置.py
@@ -0,0 +1,12 @@
+def findSubStr(substr, str, i):
+    count = 0
+    while i > 0:
+        index = str.find(substr)
+        if index == -1:
+            return -1
+        else:
+            str = str[index+1:]   #第一次出现的位置截止后的字符串
+            print(str)
+            i -= 1
+            count = count + index + 1   #字符串位置数累加
+    return count - 1
