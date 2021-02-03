c55bc8589f2c2414572d4b53765572901d42d897
周小燕 <zhouxiaoyan@ccbtrust.com.cn>
2021-02-03 14:01:09 +0800

    Linux zip命令

---
+++ linux-cmd/Linux系统如何压缩不丢失符号链接.sh
@@ -0,0 +1,9 @@
+zip -q -r -y node_modules_02.zip node_modules
+
+-q 不显示指令执行过程。
+
+-r 递归处理，将指定目录下的所有文件和子目录一并处理。
+
+-y 直接保存符号连接，而非该连接所指向的文件，本参数仅在UNIX之类的系统下有效。
+
+https://www.runoob.com/linux/linux-comm-zip.html
