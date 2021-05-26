10b6677d3950a939e0ae0fe64465b129927a1754
zhouxiaoyan <zhouxiaoyan@ccbscf.com>
2021-05-27 00:45:00 +0800

    字符串中第一次出现的字符

---
+++ python/算法/firstStr.py
@@ -0,0 +1,15 @@
+def fistshow(st):
+    st = list(st)
+    fs = {}
+    for i in range(len(st)):
+        if st[i] in fs:
+            fs[st[i]] -= 1
+        else:
+            fs[st[i]] = 1
+    for iv,v in fs.items():
+        if v == 1:
+            print(iv)
+            return iv
+
+if __name__ == "__main__":
+    fistshow("strs")
