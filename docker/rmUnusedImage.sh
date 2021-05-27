58ba686c316866553f9975271373db706aed6b2c
zhouxiaoyan <zhouxiaoyan@ccbscf.com>
2021-05-27 16:39:19 +0800

    清理容器里的日志

--- docker/rmUnusedImage.sh
+++ docker/rmUnusedImage.sh
@@ -4,3 +4,8 @@ docker rm `docker ps -a | grep Exited | awk '{print $1}'`
 docker rmi `docker images | grep  "<none>" | awk '{print $3}'`
 docker images --no-trunc # 显示完整的镜像信息
 docker system df # 用于查看Docker的磁盘使用情况
+
+#清理容器里的日志
+cd /var/lib/docker/containers
+for i in `ls`; do echo "" > $i/$i-json.log; done
+for i in `ls`; do echo "" > $i/$i.json; done
