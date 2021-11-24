1a6d4b150faeb5cfe0a0cc80f534af2ce66d761f
周小燕 <zhouxiaoyan@ccbscf.com>
2021-11-24 11:40:52 +0800

    influxdb.sh

--- linux-cmd/cgroup.sh
+++ linux-cmd/cgroup.sh
@@ -2,3 +2,4 @@ cgcreate -t jxrttest:jxrttest -a jxrttest:jxrttest -g memory:jxrttest
 echo 7G > /sys/fs/cgroup/memory/jxrttest/memory.limit_in_bytes
 cat /sys/fs/cgroup/memory/jxrttest/memory.limit_in_bytes
 cgexec -g memory:jxrttest ls
+cgexec -g memory:jxrttest nohup python3 qa_portal.py >> /var/package/portal.log 2>&1 &
