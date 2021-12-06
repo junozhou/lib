88689ecf21b07284d6c6232896c59ee01e0517ec
周小燕 <zhouxiaoyan@ccbscf.com>
2021-12-06 16:26:09 +0800

    onclick方法未定义

---
+++ mongo/分页查询排序.py
@@ -0,0 +1,17 @@
+
+import pymongo
+def page_query(query_filter=None,page_size=10,page_no=1):
+    """分页查询"""
+    myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
+    mydb = myclient["cron_status"]
+    skip = page_size*(page_no-1)
+    collection_data = mydb.get_collection("table_name").find(query_filter,{
+        #     过滤不需要返回的字段
+        #     'job_state':0,'next_run_time':0
+    }).sort([("end_date",1)]).collation({"locale": "en_US", "numericOrdering": True}).limit(page_size).skip(skip)
+    # mongo统计数据量，count方法已经废弃，用count_documents
+    itemsCount = mydb.get_collection("env_use_plan").count_documents(query_filter)
+
+if __name__ == '__main__':
+    #app = Flask(__name__)
+    page_query(None,4,1)
--- python/利用sys获取当前方法名称.py
+++ python/利用sys获取当前方法名称.py
@@ -1,4 +1,10 @@
 import sys
+
+
+funcName = sys._getframe().f_back.f_code.co_name #获取调用函数名
+lineNumber = sys._getframe().f_back.f_lineno     #获取行号
+
+
 def create():
     print(f"当前方法名：{sys._getframe().f_code.co_name}")
 
