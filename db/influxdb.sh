f18d7ef528dd393d7b98b3ca1e5dec9f1f388d82
周小燕 <zhouxiaoyan@ccbscf.com>
2022-03-23 18:51:53 +0800

    onclick方法未定义

--- db/influxdb.sh
+++ db/influxdb.sh
@@ -1,3 +1,4 @@
+from influxdb import InfluxDBClient
 
 def test():
     client = InfluxDBClient('127.0.0.1', 8086, 'root', 'root')
@@ -8,7 +9,7 @@ def test():
 
         dbName = i
         print(dbName)
-        client.switch_database(i["name")
+        client.switch_database(i["name"])
         sql = 'CREATE RETENTION POLICY "3_hours" ON "{}" DURATION 3h REPLICATION 1 DEFAULT'.format(i["name"])
         client.query(sql)
         sql1 = 'SHOW RETENTION POLICIES ON "{}"'.format(i["name"])
