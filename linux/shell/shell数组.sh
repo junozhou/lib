07f2b121848a436ebe5bc15b23cf72ae58b9ac3f
周小燕 <zhouxiaoyan@ccbscf.com>
2022-11-26 18:46:00 +0800

    shell数组.sh

---
+++ linux/shell/shell数组.sh
@@ -0,0 +1,10 @@
+数组中可以存放多个值。Bash Shell 只支持一维数组（不支持多维数组），
+初始化时不需要定义数组大小（与 PHP 类似）。
+
+与大部分编程语言类似，数组元素的下标由 0 开始。
+Shell 数组用括号来表示，元素用"空格"符号分割开，语法格式如下：
+
+array_name=(value1 value2 ... valuen)
+
+
+https://www.runoob.com/linux/linux-shell-array.html
